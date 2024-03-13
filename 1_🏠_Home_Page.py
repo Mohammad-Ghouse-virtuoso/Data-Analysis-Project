dbd33d95f775a8b1912b63f6337296a1db214d32
Shaik Mohammad Ghouse <84439948+shaikh-cyber@users.noreply.github.com>
2024-03-14 00:57:54 +0530

    Create 1_🏠_Home_Page.py
    This is a welcome page; this page must be outside of the section pages.

---
+++ 1_🏠_Home_Page.py
@@ -0,0 +1,38 @@
+import streamlit as st 
+st.markdown("""
+<style>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet">
+.css-10trblm e16nr0p30 {
+font-family:Arial, sans-serif !important;
+}
+</style>
+""",unsafe_allow_html=True)
+st.markdown("""
+<style>
+.css-1rs6os.edgvbvh3
+{
+    visibilty:hidden;
+}         
+.css-1lsmgbg.egzxvld0
+{
+    visibility:hidden; 
+}
+</style>
+            
+""",unsafe_allow_html=True)
+st.sidebar.success("Add a new page here..")
+st.title("Social Progress Index 🎗️")
+st.header("Welcome to our page, we analyse the socail progress on humanity from across the world.")
+st.markdown("---")
+width=350
+st.image(f"Representing uniity(2)_res.jpg",caption="Representing unity",use_column_width=True,width={width})
+st.markdown("**The Social Progress Index (SPI)** is a comprehensive measure of a country's social and environmental performance, providing valuable insights into the overall well-being of its citizens. By taking into account factors such as access to basic needs, education, health, and personal rights, the SPI offers a more holistic view of progress beyond traditional economic indicators. This allows policymakers and researchers to understand the strengths and weaknesses of a society, enabling them to make more informed decisions on how to improve the quality of life for its people.")
+st.markdown("One of the key advantages of the SPI is its ability to highlight areas for improvement and track progress over time. By focusing on outcomes rather than inputs, the SPI encourages a more outcomes-based approach to development that prioritizes the well-being of individuals and communities. Furthermore, the SPI provides a common framework for comparing countries and regions, facilitating international cooperation and knowledge-sharing on best practices for social development. **In a world where economic growth is often prioritized over social progress, the SPI serves as a valuable tool for promoting a more inclusive and sustainable approach to development that benefits all of humanity**.")
+st.markdown("---")
+st.caption("**Comprehensive Measurement:** The SPI rigorously measures country performance on various aspects of social and environmental performance that are relevant for countries.")
+st.caption("**Impact and Action:** The Social Progress Imperative emphasizes moving from index measurement to action and impact, focusing on how data can drive meaningful change in society.")
+st.caption("**Holistic View of Progress:** The SPI goes beyond traditional economic indicators by considering factors such as access to basic needs, education, health, and personal rights, providing a more holistic view of progress.")
+st.caption("**Role of Business Leaders:** Business leaders are encouraged to build relationships and collaborate with other sectors to drive social progress and improve societal well-being.")
+st.caption("**Mission of Social Progress Imperative:** The main mission of the Social Progress Imperative is to measure and advance social progress, providing leaders with innovative measurement tools to drive progress in business, government, and civil society.")
+st.markdown("---")
+st.caption("Embrace the transformative power of the Social Progress Index, as it illuminates pathways towards a brighter future built upon equality, inclusivity, and sustainable prosperity – unleashing the true potential of societies worldwide!")
